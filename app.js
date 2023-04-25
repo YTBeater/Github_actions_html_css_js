87401eeda037fd66716fef6ff97eb3e6e682118e
Yann <49924352+YTBeater@users.noreply.github.com>
2023-04-25 14:29:27 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
-function add(a, b) {
+export function add(a, b) {
   if (typeof a !== 'number' || typeof b !== 'number') {
     throw new Error('Les paramètres doivent être des nombres');
   }
   return a + b;
 }
-
-module.exports = add;
